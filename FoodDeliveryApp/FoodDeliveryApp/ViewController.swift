f7250b868da3273f9ab92b567d6d11ef82f898c7
STASART <121148380+STASART@users.noreply.github.com>
2024-02-02 00:47:43 +0300

    Created base project, removed storyboard reference

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Станислав Артамонов on 2.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
